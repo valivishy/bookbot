6e26b5b73ee034455529b0d4874979387dde0380
valentinvisan <valentin.visan.vv@gmail.com>
2025-04-21 18:58:23 +0300

    chore :: main.py :: adjust for simpler usage

--- main.py
+++ main.py
@@ -8,15 +8,31 @@ from stats import (
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
-    book_path = sys.argv[1]
 
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    print_report(book_path, num_words, chars_sorted_list)
+        book_path = "books/frankenstein.txt"
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        chars_dict = get_chars_dict(text)
+        chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+        print(f"--- Begin report of {book_path} ---")
+        print(f"{num_words} words found in the document")
+        print()
+
+        for item in chars_sorted_list:
+            if not item["char"].isalpha():
+                continue
+            print(f"The '{item['char']}' character was found {item['num']} times")
+
+        print("--- End report ---")
+    else:
+        book_path = sys.argv[1]
+
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        chars_dict = get_chars_dict(text)
+        chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+        print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path):
